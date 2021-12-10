e139dafecd37355813a33f5bffd2131882ccc66d
Chris F <CF-public@gmx.net>
2021-12-10 15:26:47 +0100

    Added snow cleaning speed up // added Royalty as a requirement // updated Preview.png

--- Source/HarmonyPatches.cs
+++ Source/HarmonyPatches.cs
@@ -22,6 +22,10 @@ namespace CleaningLimbs
 			harmony.Patch(
 				typeof(JobDriver_CleanFilth).GetMethod("MakeNewToils", BindingFlags.Instance | BindingFlags.NonPublic),
 				postfix: new HarmonyMethod(typeof(HarmonyPatches), nameof(HarmonyPatches.JobDriver_CleanFilth_MakeNewToils_Postfix)));
+
+			harmony.Patch(
+				typeof(JobDriver_ClearSnow).GetMethod("MakeNewToils", BindingFlags.Instance | BindingFlags.NonPublic),
+				postfix: new HarmonyMethod(typeof(HarmonyPatches), nameof(HarmonyPatches.JobDriver_ClearSnow_MakeNewToils_Postfix)));
 		}
 
 		static IEnumerable<Toil> JobDriver_CleanFilth_MakeNewToils_Postfix(IEnumerable<Toil> toils, JobDriver_CleanFilth __instance)
@@ -76,7 +80,7 @@ namespace CleaningLimbs
 						// mop feet'ing
 						if (feet > 0)
 						{
-							CleanAdjacent(toil, filth, __instance.Map, feet);
+							CleanAdjacent(toil, __instance.Map, filth, feet);
 						}
 
 						// base functionality
@@ -91,7 +95,7 @@ namespace CleaningLimbs
 			}
 
 			// function to clean adjacent tiles
-			void CleanAdjacent(Toil toil, Filth filth, Map map, int feet)
+			void CleanAdjacent(Toil toil, Map map, Filth filth, int feet)
 			{
 				var cleans = feet * 2;
 				foreach (var cell in GenAdj.AdjacentCellsAround)
@@ -125,6 +129,76 @@ namespace CleaningLimbs
 				while (!filth.Destroyed && cleans-- > 0)
 				{
 					filth.ThinFilth();
+				}
+			}
+		}
+
+
+		static IEnumerable<Toil> JobDriver_ClearSnow_MakeNewToils_Postfix(IEnumerable<Toil> toils, JobDriver_ClearSnow __instance)
+		{
+			int c = toils.Count();
+			foreach (var toil in toils)
+			{
+				if (--c == 0) // hopefully the clearing snow job will always be the last job in the list...
+					ReplaceCleanToilAction(toil);
+				yield return toil;
+			}
+
+			void ReplaceCleanToilAction(Toil toil)
+			{
+				// count number of parts ... there are probably more efficient ways to handle this
+				int hands = 0;
+				int feet = 0;
+				foreach (var part in __instance.pawn.health.hediffSet.GetHediffs<Hediff_AddedPart>())
+				{
+					var name = part.def.defName;
+					switch (name)
+					{
+						case "VacuumHand":
+							hands++;
+							break;
+						case "MopFoot":
+							feet++;
+							break;
+					}
+				}
+
+				// replace tick action
+				toil.tickAction = delegate
+				{
+					float statValue = toil.actor.GetStatValue(StatDefOf.GeneralLaborSpeed);
+					__instance.workDone += statValue + hands * 0.50f;
+					if (__instance.workDone >= __instance.TotalNeededWork)
+					{
+						var map = __instance.Map;
+						var loc = __instance.TargetLocA;
+						map.snowGrid.SetDepth(loc, 0f);
+
+						if (feet > 0)
+							CleanAdjacent(toil, map, loc, feet);
+
+						__instance.ReadyForNextToil();
+					}
+				};
+			}
+
+			// function to clean adjacent tiles
+			void CleanAdjacent(Toil toil, Map map, IntVec3 center, int feet)
+			{
+				var clears = feet * 2;
+				foreach (var cell in GenAdj.AdjacentCellsAround)
+				{
+					var loc = center + cell;
+					// search for snow
+					if (map.snowGrid.GetDepth(loc) > 0f)
+					{
+						// clear snow
+						map.snowGrid.SetDepth(loc, 0f);
+
+						// limit to max number of cleans
+						if (--clears == 0)
+							return;
+					}
 				}
 			}
 		}
